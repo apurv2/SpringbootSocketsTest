25b1c407766969d89e3bcb47ffca9e4d9b4dff48
apurv2 <apurv2@gmail.com>
2019-08-01 22:34:51 -0400

    added kotlin

---
+++ src/main/java/meetme/timeline/TimelineServiceImpl.kt
@@ -0,0 +1,39 @@
+package meetme.timeline
+
+import meetme.tweets.Followers
+import meetme.tweets.Tweet
+import meetme.tweets.TweetsService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+
+@Service
+class TimelineServiceImpl : TimelineService {
+
+    @Autowired
+    lateinit var timelineRepository: TimelineRepository
+
+    @Autowired
+    lateinit var tweetsService: TweetsService
+
+    private fun createTimelineEntries(followers: List<Followers>, tweet: Tweet) = followers.map { mapTimeline(tweet, it) }
+
+    override fun saveTimeLine(tweet: Tweet, userId: String) =
+            timelineRepository!!.save(createTimelineEntries(tweetsService!!.findFollowers(userId), tweet))!!
+
+
+    override fun getTimeline(userId: String) = timelineRepository!!.findByUserId(userId)
+
+    private fun mapTimeline(tweet: Tweet, followers: Followers): Timeline {
+        var timeline = Timeline()
+        with(Timeline()) {
+            tweetMessage = tweet.tweetMessage
+            userId = followers.childUserId
+            tweetedUserName = tweet.userName
+        }
+        return timeline
+    }
+}
+
+//        timeline.tweetMessage = tweet.tweetMessage
+//        timeline.userId = followers.childUserId
+//        timeline.tweetedUserName = tweet.userName
